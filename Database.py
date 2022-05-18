d1aa62212db2600f8396679cf6cc8f17161d72cf
Fabrice-Maison <malaboeuf.fabrice@gmail.com>
2022-05-18 21:54:00 +0200

    Database site creation Logg not finished

--- Database.py
+++ Database.py
@@ -1,15 +1,33 @@
 import sqlite3
+import Log
 
 
 class Database:
 
     def __init__(self):
+        self.data = ""
+        self.con = None
+        self.cur = None
+        logger.debug(f"__Init__")
+
+    def connect(self):
         self.con = sqlite3.connect('./database/Inventaire.db')
         self.cur = self.con.cursor()
 
-    def select_lieu(self):
-        self.cur.execute("select * from Lieu")
-        return self.cur.fetchall()
+    def find_site(self, id_site):
+        self.cur.execute(f"select * from Lieu where ID='{id_site}'")
+        lst = self.cur.fetchall()
+        if len(lst) > 0:
+            return True
+        else:
+            return False
+
+    def insert_site(self, id_site, label):
+        try:
+            self.cur.execute(f"insert into Lieu('ID','Label') values('{id_site}','{label}')")
+            self.con.commit()
+        except sqlite3.Error as err:
+            print(err.sqlite_errorname)
 
     def close(self):
         self.cur.close()
@@ -20,7 +38,7 @@ if __name__ == "__main__":
     print("test")
 
     data = Database()
-    res = data.select_lieu()
+    res = data.find_site('55')
     data.close()
     print(res)
 else:
--- LabelEdit.py
+++ LabelEdit.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtCore import QEvent, Qt
 from PyQt5.QtGui import QKeyEvent
+from Database import Database
 
 
 class LabelEdit(QLineEdit):
@@ -14,7 +15,15 @@ class LabelEdit(QLineEdit):
         if event.type() == QEvent.KeyPress:
             keyevent = QKeyEvent(event)
             if keyevent.key() == Qt.Key_Enter or keyevent.key() == Qt.Key_Return:
-                print(self.text())
+                print("ICI")
+                data = Database()
+                data.connect()
+                if data.find_site(self.text()):
+                    print("Trouvé")
+                else:
+                    print("Pas trouvé")
+                    data.insert_site(self.text(), self.text())
+                data.close()
                 self.setText("")
                 return True
             else:
